e80a07f85fca50d1bd9bec1b999f1f773d80e346
Matthias Harrer <ma.harrer@gmail.com>
2019-01-06 17:17:16 +0100

    displayed coordinates update when moving / resizing the zone

--- client/zone/js-modules/coordinate-transforms.js
+++ client/zone/js-modules/coordinate-transforms.js
@@ -1,6 +1,6 @@
 export const noTransform = new DOMMatrix([1, 0, 0, 1, 0, 0]);
 export const flipX = new DOMMatrix([1, 0, 0, -1, 0, 0]);
 
-export const transformFromMeter = noTransform
+export const transformFromMeter = new DOMMatrix([1, 0, 0, 1, 0, 0])
     .translateSelf(512, 512)
     .scaleSelf(20);
