Hypfer/Valetudo

commit 4f8409c43f7778e7b7019fd6387e4eacac875145

remove callback from pollStatusLoop

lib/devices/MiioVacuum.js
@@ -59,12 +59,12 @@ class MiioVacuum {
         );
 
         // Poll status once per minute.
-        let pollStatusLoop = function() {
+        let pollStatusLoop = () => {
             let repollSeconds =
                 MiioVacuum.FAST_MAP_PULLING_STATES.indexOf(this.status.state) !== -1 ? 2 : 60;
-            setTimeout(() => this.pollStatus(pollStatusLoop), repollSeconds * 1000);
-        }.bind(this);
-        this.pollStatus(pollStatusLoop);
+            setTimeout(pollStatusLoop, repollSeconds * 1000);
+        };
+        pollStatusLoop();
     }
 
     /**
@@ -83,7 +83,7 @@ class MiioVacuum {
         setTimeout(() => this.pollMap(), 3500);
     }
 
-    pollStatus(callback) {}
+    async pollStatus() {}
 
     /**
      * Returns the current status of the robot
@@ -140,8 +140,6 @@ class MiioVacuum {
 
     /**
      * Go back to the dock
-     * Returns an error if there is one as the first parameter of the callback
-     * On success, 2nd param of callback looks like this: 'ok'
      * @return {Promise<object>}
      */
     driveHome() {

lib/devices/Roborock.js
@@ -133,8 +133,8 @@ class Roborock extends MiioVacuum {
         this.events.emit("miio.status", this.status);
     }
 
-    pollStatus(callback) {
-        this.getCurrentStatus(callback);
+    async pollStatus() {
+        await this.getCurrentStatus();
     }
 
     /**
@@ -472,7 +472,7 @@ class Roborock extends MiioVacuum {
     *      fan_power: 60,
     *  }
     */
-    getCurrentStatus(callback) {
+    getCurrentStatus() {
         return this.sendLocal("get_status", {}).then((res) => {
             if (res) {
                 this.updateStatus(res[0]);

lib/devices/Viomi.js
@@ -75,13 +75,11 @@ class Viomi extends MiioVacuum {
         this.events.emit("miio.status", this.status);
     }
 
-    pollStatus(callback) {
-        this.sendLocal("get_prop", STATE_PROPERTIES).then((res) => {
-            let statusDict = {};
-            STATE_PROPERTIES.forEach((key, index) => statusDict[key] = res[index]);
-            this.updateStatus(statusDict);
-            callback();
-        });
+    async pollStatus() {
+        let res = await this.sendLocal("get_prop", STATE_PROPERTIES);
+        let statusDict = {};
+        STATE_PROPERTIES.forEach((key, index) => statusDict[key] = res[index]);
+        this.updateStatus(statusDict);
     }
 
     getCurrentStatus() {

commit 4cce7c9acd9a16654fd990605f31d3056349fb9f

Actually poll status in pollStatusLoop (broken by 4f8409c)

lib/devices/MiioVacuum.js
@@ -58,13 +58,13 @@ class MiioVacuum {
             options.tokenProvider
         );
 
-        // Poll status once per minute.
+        // Poll status once per minute (or more frequent during operation)
         let pollStatusLoop = () => {
             let repollSeconds =
                 MiioVacuum.FAST_MAP_PULLING_STATES.indexOf(this.status.state) !== -1 ? 2 : 60;
-            setTimeout(pollStatusLoop, repollSeconds * 1000);
+            setTimeout(() => this.pollStatus().finally(pollStatusLoop), repollSeconds * 1000);
         };
-        pollStatusLoop();
+        this.pollStatus().finally(pollStatusLoop);
     }
 
     /**