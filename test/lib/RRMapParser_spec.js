d21b342b5f957e256440dc4641a8df043a3a827b
Alexander Knöbel <alex-git@knoebel.info>
2020-04-11 22:20:56 +0200

    rename test to better match data

--- test/lib/RRMapParser_spec.js
+++ test/lib/RRMapParser_spec.js
@@ -1,21 +1,25 @@
 const fs =  require("fs").promises;
-require("should");
+const should = require("should");
 
 const RRMapParser = require("../../lib/RRMapParser");
 
 describe("RRMapParser", function () {
-    it("should parse map from firmware 1886 correctly", async function() {
-        let data = await fs.readFile("./test/lib/RRMapParser/FW1886.bin");
-        let expected = JSON.parse(await fs.readFile("./test/lib/RRMapParser/FW1886.json", { encoding: "utf-8" }));
+    it("should parse map without extra data from firmware 1886 correctly", async function() {
+        let data = await fs.readFile("./test/lib/RRMapParser/FW1886_without_extra_data.bin");
+        let expected = JSON.parse(await fs.readFile("./test/lib/RRMapParser/FW1886_without_extra_data.json", { encoding: "utf-8" }));
 
         let actual = RRMapParser.PARSE(data);
 
-        actual.image.position.should.deepEqual(expected.image.position);
-        actual.image.dimensions.should.deepEqual(expected.image.dimensions);
-        actual.image.pixels.should.deepEqual(expected.image.pixels);
-        actual.path.should.deepEqual(expected.path);
-        actual.charger.should.deepEqual(expected.charger);
-        actual.robot.should.deepEqual(expected.robot);
-        actual.should.deepEqual(expected);
+        actual.image.position.should.deepEqual(expected.image.position, "position");
+        actual.image.dimensions.should.deepEqual(expected.image.dimensions, "dimensions");
+        actual.image.pixels.should.deepEqual(expected.image.pixels, "pixels");
+        actual.path.should.deepEqual(expected.path, "path");
+        actual.charger.should.deepEqual(expected.charger, "charger");
+        actual.robot.should.deepEqual(expected.robot, "robot");
+        should.not.exists(actual.goto_target, "goto_target");
+        should.not.exists(actual.currently_cleaned_zones, "currently_cleaned_zones");
+        should.not.exists(actual.forbidden_zones, "forbidden_zones");
+        should.not.exists(actual.virtual_walls, "virtual_walls");
+        actual.should.deepEqual(expected, "full");
     });
 });
