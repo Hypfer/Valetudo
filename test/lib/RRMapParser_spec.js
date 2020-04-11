31726634dee5786694078e2c2a5ede501f76cb37
Alexander Knöbel <alex-git@knoebel.info>
2020-04-11 23:22:02 +0200

    add test for goto_target

--- test/lib/RRMapParser_spec.js
+++ test/lib/RRMapParser_spec.js
@@ -41,4 +41,23 @@ describe("RRMapParser", function () {
         should.deepEqual(actual.virtual_walls, expected.virtual_walls, "virtual_walls");
         actual.should.deepEqual(expected, "full");
     });
+
+    it("should parse map with goto_target from firmware 1886 correctly", async function() {
+        let data = await fs.readFile("./test/lib/RRMapParser/FW1886_with_goto_target.bin");
+        let expected = JSON.parse(await fs.readFile("./test/lib/RRMapParser/FW1886_with_goto_target.json", { encoding: "utf-8" }));
+
+        let actual = RRMapParser.PARSE(data);
+
+        actual.image.position.should.deepEqual(expected.image.position, "position");
+        actual.image.dimensions.should.deepEqual(expected.image.dimensions, "dimensions");
+        actual.image.pixels.should.deepEqual(expected.image.pixels, "pixels");
+        actual.path.should.deepEqual(expected.path, "path");
+        actual.charger.should.deepEqual(expected.charger, "charger");
+        actual.robot.should.deepEqual(expected.robot, "robot");
+        should.deepEqual(actual.goto_target, expected.goto_target, "goto_target");
+        should.not.exists(actual.currently_cleaned_zones, "currently_cleaned_zones");
+        should.not.exists(actual.no_go_areas, "no_go_areas");
+        should.not.exists(actual.virtual_walls, "virtual_walls");
+        actual.should.deepEqual(expected, "full");
+    });
 });
