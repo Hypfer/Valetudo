5d3ab1c9803d1deb956adf9026983993d9c3613b
Alexander Knöbel <alex-git@knoebel.info>
2020-04-12 00:10:36 +0200

    add support for firmware 2008 maps

--- test/lib/RRMapParser_spec.js
+++ test/lib/RRMapParser_spec.js
@@ -60,4 +60,23 @@ describe("RRMapParser", function () {
         should.not.exists(actual.virtual_walls, "virtual_walls");
         actual.should.deepEqual(expected, "full");
     });
+
+    it("should parse map with forbidden_zones,virtual_walls,currently_cleaned_zones from firmware 2008 correctly", async function() {
+        let data = await fs.readFile("./test/lib/RRMapParser/FW2008_with_forbidden_zones_and_virtual_walls_and_currently_cleaned_zones.bin");
+        let expected = JSON.parse(await fs.readFile("./test/lib/RRMapParser/FW2008_with_forbidden_zones_and_virtual_walls_and_currently_cleaned_zones.json", { encoding: "utf-8" }));
+
+        let actual = RRMapParser.PARSE(data);
+
+        actual.image.position.should.deepEqual(expected.image.position, "position");
+        actual.image.dimensions.should.deepEqual(expected.image.dimensions, "dimensions");
+        actual.image.pixels.should.deepEqual(expected.image.pixels, "pixels");
+        actual.path.should.deepEqual(expected.path, "path");
+        actual.charger.should.deepEqual(expected.charger, "charger");
+        actual.robot.should.deepEqual(expected.robot, "robot");
+        should.not.exists(actual.goto_target, "goto_target");
+        should.deepEqual(actual.currently_cleaned_zones, expected.currently_cleaned_zones, "currently_cleaned_zones");
+        should.deepEqual(actual.no_go_areas, expected.no_go_areas, "no_go_areas");
+        should.deepEqual(actual.virtual_walls, expected.virtual_walls, "virtual_walls");
+        actual.should.deepEqual(expected, "full");
+    });
 });
