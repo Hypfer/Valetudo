6724e5556f3350c1c3cc6d4d5f09a9e08e8f2a6b
Hypfer <soeren@soerenbeye.de>
2020-05-28 15:40:05 +0200

    fix(ui): Fixed virtual wall placement

--- client/zone/js-modules/locations.js
+++ client/zone/js-modules/locations.js
@@ -279,11 +279,11 @@ export class VirtualWall  {
             this.active = false;
         }
 
-        this.x1 = Math.min(x1, x2);
-        this.x2 = Math.max(x1, x2);
+        this.x1 = x1;
+        this.x2 = x2;
 
-        this.y1 = Math.min(y1, y2);
-        this.y2 = Math.max(y1, y2);
+        this.y1 = y1;
+        this.y2 = y2;
     }
 
     draw(ctx, transformFromMapSpace) {
