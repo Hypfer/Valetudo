b6e77075b0e7178e44c67428b5bf9953ee1c4376
Aaron Korte <github@aaronkorte.xyz>
2020-07-15 18:53:23 +0200

    Implement pixel skips
    Consumers can now specify a resolution to improve performance of segment boundary search.

--- client/zone/js-modules/map-color-finder.js
+++ client/zone/js-modules/map-color-finder.js
@@ -7,8 +7,14 @@ export class FourColorTheoremSolver {
      * Each pair of segments is then represented as an edge in a graph where the vertices represent the segments themselves.
      * We then use a simple greedy algorithm to color all vertices so that none of its edges connect it to a vertex with the same color.
      * @param {Array<object>} layers - the data containing the map image (array of pixel offsets)
+     * @param {number} resolution - Minimal resolution of the map scanner in pixels. Any number higher than one will lead to this many pixels being skipped when finding segment boundaries.
+     * For example: If the robot measures 30cm in length/width, this should be set to 6, as no room can be smaller than 6 pixels. This of course implies that a pixel represents 5cm in the real world.
      */
-    constructor(layers) {
+    constructor(layers, resolution) {
+        const prec = Math.floor(resolution);
+        this.stepFunction = function(c) {
+            return c + prec;
+        };
         var preparedLayers = this.preprocessLayers(layers);
         var map = this.createPixelToSegmentMapping(preparedLayers);
         this.areaGraph = this.buildGraph(map);
@@ -102,16 +108,16 @@ export class FourColorTheoremSolver {
 
     traverseMap(boundaries, pixelData, func) {
         // row-first traversal
-        for (let y = boundaries.minY; y <= boundaries.maxY; y++) {
+        for (let y = boundaries.minY; y <= boundaries.maxY; y = this.stepFunction(y)) {
             var rowFirstSegmentId = undefined;
-            for (let x = boundaries.minX; x <= boundaries.maxX; x++) {
+            for (let x = boundaries.minX; x <= boundaries.maxX; x = this.stepFunction(x)) {
                 rowFirstSegmentId = func(x, y, rowFirstSegmentId, pixelData);
             }
         }
         // column-first traversal
-        for (let x = boundaries.minX; x <= boundaries.maxX; x++) {
+        for (let x = boundaries.minX; x <= boundaries.maxX; x = this.stepFunction(x)) {
             var colFirstSegmentId = undefined;
-            for (let y = boundaries.minY; y <= boundaries.maxY; y++) {
+            for (let y = boundaries.minY; y <= boundaries.maxY; y = this.stepFunction(y)) {
                 colFirstSegmentId = func(x, y, colFirstSegmentId, pixelData);
             }
         }
