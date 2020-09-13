42dd97049123106bdddabd6886bef9ed9312db34
Duarte David <deltaduartedavid@gmail.com>
2020-09-13 18:13:29 +0200

    ui: Add tailwindcss

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -9,6 +9,12 @@
 </template>
 
 <style>
+@tailwind base;
+
+@tailwind components;
+
+@tailwind utilities;
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ ui/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: [
+    './src/**/*.vue'
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
