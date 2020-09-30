Hypfer/Valetudo

commit eb6bece2ea306a6031ad0f5159c34af17b009b8c

refactor: Core Vacuum + Map Structure Rework (#544)
Furthermore:

* Fixed roborock map repolling at insane intervals

* Fixed Valetudo crash on broken websocket connection

* Calculate MapLayer area on ValetudoMap creation

client/home.js
@@ -87,7 +87,7 @@ async function handleFanspeedButton() {
     let index = await ons.openActionSheet({
         title: "Select power mode",
         cancelable: true,
-        buttons: [...Object.values(fanspeedPresets), {label: "Cancel", icon: "md-close"}]
+        buttons: [...Object.keys(fanspeedPresets), {label: "Cancel", icon: "md-close"}]
     });
 
     var level = Object.keys(fanspeedPresets)[index];
@@ -216,64 +216,118 @@ async function updateHomePage() {
     loadingBarHome.setAttribute("indeterminate", "indeterminate");
 
     try {
-        let res = await ApiService.getCurrentStatus();
+        let res = await ApiService.getVacuumState();
         loadingBarHome.removeAttribute("indeterminate");
         fanspeedButton.removeAttribute("disabled");
         findRobotButton.removeAttribute("disabled");
         spotButton.removeAttribute("disabled");
 
-        if (res.battery) {
-            batteryStatusText.innerHTML = "Battery: " + res.battery + "%";
-            batteryStatusBar.value = res.battery;
+        const buttonMap = {
+            start: startButton,
+            pause: pauseButton,
+            stop: stopButton,
+            home: homeButton,
+            spot: spotButton,
+            find: findRobotButton,
+            go_to: goToButton,
+            zones: areaButton
+        };
+
+        const buttonStateMap = { //true = enabled
+            start: true,
+            pause: true,
+            stop: true,
+            home: true,
+            spot: true,
+            find: true,
+            go_to: true,
+            zones: true
+        };
+
+        var BatteryStateAttribute = res.find(e => e.__class === "BatteryStateAttribute");
+        var StatusStateAttribute = res.find(e => e.__class === "StatusStateAttribute");
+        var AreaCleanupStatsAttribute = res.find(e => e.__class === "LatestCleanupStatisticsAttribute" && e.type === "area");
+        var DurationCleanupStatsAttribute = res.find(e => e.__class === "LatestCleanupStatisticsAttribute" && e.type === "duration");
+        var FanSpeedStateAttribute = res.find(e => e.__class === "FanSpeedStateAttribute");
+
+        if (BatteryStateAttribute) {
+            batteryStatusText.innerText = "Battery: " + BatteryStateAttribute.level + "%";
+            batteryStatusBar.value = BatteryStateAttribute.level;
         }
 
-        robotState.innerHTML = res.human_state;
-        if (res.error_code) {
-            robotState.innerHTML +=
-                "<span class=\"robot-error\"><ons-icon icon=\"fa-exclamation-triangle\"></ons-icon> " +
-                res.human_error +
-                " <ons-icon icon=\"fa-exclamation-triangle\"></ons-icon></span>";
-        }
+        if (StatusStateAttribute) {
+            robotState.innerText = StatusStateAttribute.value;
 
-        if (res.in_cleaning === 1 || res.in_cleaning === 2 ||
-            ["SPOT_CLEANING", "GOING_TO_TARGET", "CLEANING"].indexOf(res.state) != -1) {
-            if (["IDLE", "PAUSED", "CHARGER_DISCONNECTED"].indexOf(res.state) != -1) {
-                pauseButton.setAttribute("disabled", "disabled");
-                startButton.removeAttribute("disabled");
-            } else {
-                pauseButton.removeAttribute("disabled");
-                startButton.setAttribute("disabled", "disabled");
+            if (StatusStateAttribute.flag !== "none") {
+                robotState.innerText += " " + StatusStateAttribute.flag;
             }
-            spotButton.setAttribute("disabled", "disabled");
-        } else {
-            spotButton.removeAttribute("disabled");
-            pauseButton.setAttribute("disabled", "disabled");
-            if (res.state !== 6) {
-                startButton.removeAttribute("disabled");
-            } else {
-                pauseButton.removeAttribute("disabled");
-                startButton.setAttribute("disabled", "disabled");
+
+            if (StatusStateAttribute.value === "error") {
+                robotState.innerHTML +=
+                    "<span class=\"robot-error\"><ons-icon icon=\"fa-exclamation-triangle\"></ons-icon> " +
+                    StatusStateAttribute.metaData.error_description +
+                    " <ons-icon icon=\"fa-exclamation-triangle\"></ons-icon></span>";
+
+            }
+
+            switch (StatusStateAttribute.value) {
+                case "docked":
+                    buttonStateMap.pause = false;
+                    buttonStateMap.stop = false;
+                    buttonStateMap.home = false;
+                    buttonStateMap.spot = false;
+                    break;
+                case "idle":
+                    buttonStateMap.pause = false;
+                    buttonStateMap.stop = false;
+                    break;
+                case "returning":
+                    buttonStateMap.start = false;
+                    buttonStateMap.home = false;
+                    buttonStateMap.go_to = false;
+                    buttonStateMap.zones = false;
+                    break;
+                case "cleaning":
+                    buttonStateMap.start = false;
+                    buttonStateMap.home = false;
+                    buttonStateMap.spot = false;
+                    buttonStateMap.go_to = false;
+                    buttonStateMap.zones = false;
+                    break;
+                case "paused":
+                    buttonStateMap.stop = false;
+                    break;
             }
         }
 
-        if (["CHARGING", "DOCKING", "PAUSED", "IDLE"].indexOf(res.state) != -1) {
-            stopButton.setAttribute("disabled", "disabled");
-        } else {
-            stopButton.removeAttribute("disabled");
+        if (AreaCleanupStatsAttribute) {
+            robotStateDetailsM2.innerHTML = "Area: " +
+                ("00" + (AreaCleanupStatsAttribute.value / 10000).toFixed(2)).slice(-6) + " m²";
         }
 
-        if (["RETURNING_HOME", "CHARGING", "CHARGING_PROBLEM"].indexOf(res.state) != -1) {
-            homeButton.setAttribute("disabled", "disabled");
-        } else {
-            homeButton.removeAttribute("disabled");
+        if (DurationCleanupStatsAttribute) {
+            robotStateDetailsTime.innerHTML = "Time: " + secondsToHms(DurationCleanupStatsAttribute.value);
         }
 
-        robotStateDetailsM2.innerHTML =
-            "Area: " + ("00" + (res.clean_area / 1000000).toFixed(2)).slice(-6) + " m²";
-        robotStateDetailsTime.innerHTML = "Time: " + secondsToHms(res.clean_time);
-        fanspeedButton.innerHTML =
-            "<ons-icon icon=\"fa-superpowers\"></ons-icon> " +
-            (fanspeedPresets[res.fan_power] || `Custom ${res.fan_power}%`);
+        if (FanSpeedStateAttribute) {
+            fanspeedButton.innerHTML = "<ons-icon icon=\"fa-superpowers\"></ons-icon> ";
+
+            if (FanSpeedStateAttribute.value === "custom") {
+                fanspeedButton.innerHTML += `Custom ${FanSpeedStateAttribute.customValue}%`;
+            } else {
+                fanspeedButton.innerHTML += FanSpeedStateAttribute.value;
+            }
+        }
+
+        Object.keys(buttonStateMap).forEach(k => {
+            const button = buttonMap[k];
+
+            if (buttonStateMap[k]) {
+                button.removeAttribute("disabled");
+            } else {
+                button.setAttribute("disabled", "disabled");
+            }
+        });
 
         currentRefreshTimer =
             window.setTimeout(function() {

commit 1114ff86c8cc7d54abf608309a5415daff54e738

fix(viomi): pause handling that was broken by eb6bece2.
Addresses status handling (#591 & #594)

client/home.js
@@ -283,19 +283,18 @@ async function updateHomePage() {
                     break;
                 case "returning":
                     buttonStateMap.start = false;
+                    buttonStateMap.pause = false;
                     buttonStateMap.home = false;
                     buttonStateMap.go_to = false;
-                    buttonStateMap.zones = false;
                     break;
                 case "cleaning":
                     buttonStateMap.start = false;
                     buttonStateMap.home = false;
                     buttonStateMap.spot = false;
                     buttonStateMap.go_to = false;
-                    buttonStateMap.zones = false;
                     break;
                 case "paused":
-                    buttonStateMap.stop = false;
+                    buttonStateMap.pause = false;
                     break;
             }
         }

lib/devices/Viomi.js
@@ -91,6 +91,14 @@ class Viomi extends MiioVacuum {
         /** @type {BoxType} */
         this.box_type = BoxType.VACUUM;
 
+        /**
+         * Current cleaning operation.
+         * [mode, pause/start (, opt: roomspec)]
+         *
+         * mode reflects this.mode
+         */
+        this.operation = [ViomiMovementMode.NORMAL_CLEANING, 1, 0];
+
         // Default the language to EN
         this.sendCommand("set_language", [2]);
 
@@ -525,41 +533,35 @@ class Viomi extends MiioVacuum {
     }
 
     /**
+     * Sets a new operation mode.
+     *
      * @private
      *
      * @param {object} options
      * @param {ViomiMovementMode} [options.movementMode] //If unset, we'll use the current value
      * @param {ViomiOperation} options.operation //Stop/Start/Pause
-     * @param {Array} [options.additionalParameters]
-     *
-     * @returns {Promise<object>}
+     * @param {Array} [options.roomIds]
      */
-    startOperation(options) {
-        let command = "set_mode";
-        let operation = [undefined, options.operation];
+    setOperation(options) {
+        let movementMode = options.movementMode === undefined ? this.getCurrentMovementMode() : options.movementMode;
+        let operation = [movementMode, options.operation];
 
-        if (options.movementMode === undefined) {
-            operation[0] = this.getCurrentMovementMode();
-        } else {
-            operation[0] = options.movementMode;
+        if (Array.isArray(options.roomIds)) {
+            operation.push(options.roomIds.length);
+            operation = operation.concat(options.roomIds);
         }
-
-        if (Array.isArray(options.additionalParameters)) {
-            command = "set_mode_withroom";
-            operation.push(options.additionalParameters.length);
-            operation = operation.concat(options.additionalParameters);
-        }
-
-        return this.sendCommand(command, operation);
-
+        this.operation = operation;
     }
 
-    async startCleaning() {
-        await this.startOperation({
-            operation: ViomiOperation.START,
-            additionalParameters: []
-            //Intentional empty array to force set_mode_withroom command since the viomi api is utterly broken
-        });
+    /**
+     * Starts / continues an operation previously declared with setOperation.
+     *
+     * @private
+     * @returns {Promise<object>}
+     */
+    startOperation() {
+        let command = this.operation.length > 2 ? "set_mode_withroom" : "set_mode";
+        return this.sendCommand(command, this.operation);
     }
 
     /** @param {Array<number>} zone_ids */
@@ -575,9 +577,9 @@ class Viomi extends MiioVacuum {
         }
 
         if (zone_ids_from_map.length) {
-            await this.startOperation({
+            this.setOperation({
                 operation: ViomiOperation.START,
-                additionalParameters: zone_ids_from_map
+                roomIds: zone_ids_from_map
             });
         } else if (zones_from_user.length) {
             const areas = [];
@@ -589,13 +591,14 @@ class Viomi extends MiioVacuum {
 
             await this.sendCommand("set_zone", args.concat(specs));
 
-            await this.startOperation({
+            this.setOperation({
                 operation: ViomiOperation.START,
                 movementMode: ViomiMovementMode.ZONED_CLEAN_OR_MOPPING
             });
         } else {
             throw new Error("No zones to clean");
         }
+        await this.startOperation();
     }
 
     /**
@@ -605,28 +608,45 @@ class Viomi extends MiioVacuum {
      * @returns {Promise<void>}
      */
     async startCleaningSegments(segment_ids) {
-        if (segment_ids.length) {
-            this.startOperation({
-                operation: ViomiOperation.START,
-                additionalParameters: segment_ids
-            });
-        } else {
+        if (!segment_ids.length) {
             throw new Error("No segments to clean");
         }
+        this.setOperation({
+            operation: ViomiOperation.START,
+            roomIds: segment_ids
+        });
+        await this.startOperation();
+    }
+
+    async startCleaning() {
+        // If we previously stopped an operation, we do a fresh start.
+        if (this.operation[0] === ViomiOperation.STOP) {
+            // Use an empty set of rooms to clean all.
+            this.setOperation({operation: ViomiOperation.START, roomIds: []});
+        } else {
+            // Intentionally not changing any of the other operation params, so
+            // that this continues the previous operation. A reset of those params
+            // happens only via stopCleaning or starting room or zone cleaning.
+            this.operation[1] = ViomiOperation.START;
+        }
+        await this.startOperation();
     }
 
     async pauseCleaning() {
-        await this.startOperation({operation: ViomiOperation.PAUSE});
+        // Intentionally not changing any of the other operation params,
+        // see startCleaning().
+        this.operation[1] = ViomiOperation.PAUSE;
+        this.startOperation();
     }
 
     async stopCleaning() {
-        await this.startOperation({operation: ViomiOperation.STOP});
+        this.setOperation({operation: ViomiOperation.STOP});
+        await this.startOperation();
     }
 
     /** @override */
     async getFanSpeeds() {
         Logger.info("Get FanSpeeds");
-
         return this.fanSpeeds;
     }
 
@@ -700,10 +720,11 @@ const STATUS_PROP_MAP = Object.freeze({
         value: stateAttrs.StatusStateAttribute.VALUE.IDLE
     },
     1: {
-        value: stateAttrs.StatusStateAttribute.VALUE.PAUSED
-    },
-    2: {
         value: stateAttrs.StatusStateAttribute.VALUE.IDLE
+    },
+    // Paused during full clean.
+    2: {
+        value: stateAttrs.StatusStateAttribute.VALUE.PAUSED
     },
     3: {
         value: stateAttrs.StatusStateAttribute.VALUE.CLEANING