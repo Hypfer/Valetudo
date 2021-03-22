cc66d5292f1ba0306e8b5362b63860d177fb5b0f
Frank Wammes <FrankZZ@users.noreply.github.com>
2021-03-22 22:42:34 +0100

    feat(ui): Toggle button states depending on Robot Capabilities (#769)
    * feat(ui): Take robot capabilities into account when showing buttons on Homepage
    
    * feat(ui): capitalize WiFi status
    
    * feat(ui): hide buttons depending on capabilities
    
    * chore: fix linting issues
    
    * chore: remove obsolete comment
    
    * fix(ui): mqtt & access-control settings should always be enabled

---
+++ client/settings.js
@@ -0,0 +1,37 @@
+/*global ons */
+import {ApiService} from "./services/api.service.js";
+
+async function updateSettingsPage() {
+    try {
+        const robotCapabilities = await ApiService.getCapabilities() || [];
+        const buttonStateMap = {
+            info: true,
+            timers: robotCapabilities.includes("DoNotDisturbCapability"),
+            "carpet-mode": robotCapabilities.includes("CarpetModeControlCapability"),
+            "cleaning-history": false, // commented out in settings.html?
+            "persistent-data": robotCapabilities.includes("PersistentMapControlCapability"),
+            consumables: robotCapabilities.includes("ConsumableMonitoringCapability"),
+            wifi: robotCapabilities.includes("WifiConfigurationCapability"),
+            mqtt: true,
+            token: false, // commented out in settings.html?
+            sound: robotCapabilities.includes("SpeakerVolumeControlCapability"),
+            "access-control": true
+        };
+
+        Object.keys(buttonStateMap).forEach((key) => {
+            const state = buttonStateMap[key];
+            const element = document.getElementById(`settings-${key}`);
+
+            if (element && !state) {
+                element.style = "display: none;";
+            }
+
+        });
+    } catch (err) {
+        ons.notification.toast(`Error: ${err.message}`,
+            { buttonLabel: "Dismiss", timeout: window.fn.toastErrorTimeout });
+    }
+}
+
+
+window.updateSettingsPage = updateSettingsPage;
