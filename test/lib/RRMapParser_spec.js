835befab88a5531d6de14437a96ece08c5a03347
Sören Beye <github@hypfer.de>
2021-05-12 13:11:30 +0200

    test(vendor.roborock): Add S6 testcase with no-mop-zones and active segments

--- test/lib/RRMapParser_spec.js
+++ test/lib/RRMapParser_spec.js
@@ -6,10 +6,17 @@ const RRMapParser = require("../../lib/robots/roborock/RRMapParser");
 should.config.checkProtoEql = false;
 
 describe("RRMapParser", function () {
-    it("should parse map without extra data from firmware 1886 correctly", async function() {
+    it("should not parse random junk", async function() {
+        let data = Buffer.allocUnsafe(64);
+        let actual = RRMapParser.PARSE(data);
 
-        let data = await fs.readFile("./test/lib/RRMapParser/FW1886_without_extra_data.bin");
-        let expected = JSON.parse(await fs.readFile("./test/lib/RRMapParser/FW1886_without_extra_data.json", { encoding: "utf-8" }));
+        should(actual).equal(null);
+    });
+
+    it("should parse s5 map without extra data from firmware 1886 correctly", async function() {
+
+        let data = await fs.readFile("./test/lib/RRMapParser/S5_FW1886_without_extra_data.bin");
+        let expected = JSON.parse(await fs.readFile("./test/lib/RRMapParser/S5_FW1886_without_extra_data.json", { encoding: "utf-8" }));
 
         let actual = RRMapParser.PARSE(data);
 
@@ -28,9 +35,9 @@ describe("RRMapParser", function () {
         actual.should.deepEqual(expected);
     });
 
-    it("should parse map with forbidden_zones,virtual_walls,currently_cleaned_zones from firmware 1886 correctly", async function() {
-        let data = await fs.readFile("./test/lib/RRMapParser/FW1886_with_forbidden_zones_and_virtual_walls_and_currently_cleaned_zones.bin");
-        let expected = JSON.parse(await fs.readFile("./test/lib/RRMapParser/FW1886_with_forbidden_zones_and_virtual_walls_and_currently_cleaned_zones.json", { encoding: "utf-8" }));
+    it("should parse s5 map with forbidden_zones,virtual_walls,currently_cleaned_zones from firmware 1886 correctly", async function() {
+        let data = await fs.readFile("./test/lib/RRMapParser/S5_FW1886_with_forbidden_zones_and_virtual_walls_and_currently_cleaned_zones.bin");
+        let expected = JSON.parse(await fs.readFile("./test/lib/RRMapParser/S5_FW1886_with_forbidden_zones_and_virtual_walls_and_currently_cleaned_zones.json", { encoding: "utf-8" }));
 
         let actual = RRMapParser.PARSE(data);
 
@@ -49,9 +56,9 @@ describe("RRMapParser", function () {
         actual.should.deepEqual(expected);
     });
 
-    it("should parse map with goto_target from firmware 1886 correctly", async function() {
-        let data = await fs.readFile("./test/lib/RRMapParser/FW1886_with_goto_target.bin");
-        let expected = JSON.parse(await fs.readFile("./test/lib/RRMapParser/FW1886_with_goto_target.json", { encoding: "utf-8" }));
+    it("should parse s5 map with goto_target from firmware 1886 correctly", async function() {
+        let data = await fs.readFile("./test/lib/RRMapParser/S5_FW1886_with_goto_target.bin");
+        let expected = JSON.parse(await fs.readFile("./test/lib/RRMapParser/S5_FW1886_with_goto_target.json", { encoding: "utf-8" }));
 
         let actual = RRMapParser.PARSE(data);
 
@@ -70,9 +77,9 @@ describe("RRMapParser", function () {
         actual.should.deepEqual(expected);
     });
 
-    it("should parse map with forbidden_zones,virtual_walls,currently_cleaned_zones from firmware 2008 correctly", async function() {
-        let data = await fs.readFile("./test/lib/RRMapParser/FW2008_with_forbidden_zones_and_virtual_walls_and_currently_cleaned_zones.bin");
-        let expected = JSON.parse(await fs.readFile("./test/lib/RRMapParser/FW2008_with_forbidden_zones_and_virtual_walls_and_currently_cleaned_zones.json", { encoding: "utf-8" }));
+    it("should parse s5 map with forbidden_zones,virtual_walls,currently_cleaned_zones from firmware 2008 correctly", async function() {
+        let data = await fs.readFile("./test/lib/RRMapParser/S5_FW2008_with_forbidden_zones_and_virtual_walls_and_currently_cleaned_zones.bin");
+        let expected = JSON.parse(await fs.readFile("./test/lib/RRMapParser/S5_FW2008_with_forbidden_zones_and_virtual_walls_and_currently_cleaned_zones.json", { encoding: "utf-8" }));
 
         let actual = RRMapParser.PARSE(data);
 
@@ -91,9 +98,9 @@ describe("RRMapParser", function () {
         actual.should.deepEqual(expected);
     });
 
-    it("should parse map with segments from firmware 2008 correctly", async function() {
-        let data = await fs.readFile("./test/lib/RRMapParser/FW2008_with_segments.bin");
-        let expected = JSON.parse(await fs.readFile("./test/lib/RRMapParser/FW2008_with_segments.json", { encoding: "utf-8" }));
+    it("should parse s5 map with segments from firmware 2008 correctly", async function() {
+        let data = await fs.readFile("./test/lib/RRMapParser/S5_FW2008_with_segments.bin");
+        let expected = JSON.parse(await fs.readFile("./test/lib/RRMapParser/S5_FW2008_with_segments.json", { encoding: "utf-8" }));
 
         let actual = RRMapParser.PARSE(data);
 
@@ -112,9 +119,30 @@ describe("RRMapParser", function () {
         actual.should.deepEqual(expected);
     });
 
-    it("should parse map with segments from firmware 2020 correctly", async function() {
-        let data = await fs.readFile("./test/lib/RRMapParser/FW2020_with_segments.bin");
-        let expected = JSON.parse(await fs.readFile("./test/lib/RRMapParser/FW2020_with_segments.json", { encoding: "utf-8" }));
+    it("should parse s5 map with segments from firmware 2020 correctly", async function() {
+        let data = await fs.readFile("./test/lib/RRMapParser/S5_FW2020_with_segments.bin");
+        let expected = JSON.parse(await fs.readFile("./test/lib/RRMapParser/S5_FW2020_with_segments.json", { encoding: "utf-8" }));
+
+        let actual = RRMapParser.PARSE(data);
+
+        actual.layers.length.should.equal(expected.layers.length, "layerCount");
+
+        actual.layers.forEach((layer, i) => {
+            actual.layers[i].should.deepEqual(expected.layers[i]);
+        });
+
+        actual.entities.length.should.equal(expected.entities.length, "entitiesCount");
+
+        actual.entities.forEach((layer, i) => {
+            actual.entities[i].should.deepEqual(expected.entities[i]);
+        });
+
+        actual.should.deepEqual(expected);
+    });
+
+    it("should parse s6 map with segments from firmware 2652 with active segments and no-mop-zones correctly", async function() {
+        let data = await fs.readFile("./test/lib/RRMapParser/S6_FW2652_with_active_segment_and_no_mop_zone.bin");
+        let expected = JSON.parse(await fs.readFile("./test/lib/RRMapParser/S6_FW2652_with_active_segment_and_no_mop_zone.json", { encoding: "utf-8" }));
 
         let actual = RRMapParser.PARSE(data);
 
