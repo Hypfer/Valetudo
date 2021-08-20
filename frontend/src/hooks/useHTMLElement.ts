ae12ef7505be12a7984af75c63d2b75cf232870d
Sören Beye <github@hypfer.de>
2021-08-20 18:16:58 +0200

    Make deepscan happy

--- frontend/src/hooks/useHTMLElement.ts
+++ frontend/src/hooks/useHTMLElement.ts
@@ -20,7 +20,7 @@ export const useHTMLElement = <E extends HTMLElement, T>(
       window.addEventListener('resize', update);
 
       return () => {
-        window.addEventListener('resize', update);
+        window.removeEventListener('resize', update);
         cancel = true;
       };
     }
