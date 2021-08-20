63c5ee13ae7e94a1a60c37f605ea05407452d0ee
Sören Beye <github@hypfer.de>
2021-08-20 19:11:12 +0200

    Remove web vitals

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,7 @@
 import ReactDOM from 'react-dom';
 import '@fontsource/roboto';
-import reportWebVitals from './reportWebVitals';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
