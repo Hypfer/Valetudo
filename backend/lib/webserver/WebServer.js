c5b809fed6b00d8a7fd6e7e2f517793dcb8e5057
Sören Beye <github@hypfer.de>
2021-08-21 14:12:43 +0200

    Allow the client-side router to work

--- backend/lib/webserver/WebServer.js
+++ backend/lib/webserver/WebServer.js
@@ -122,6 +122,9 @@ class WebServer {
         this.app.use(express.static(path.join(__dirname, "../../..", "old_frontend/lib")));
 
         this.app.use("/new_frontend", express.static(path.join(__dirname, "../../..", "frontend/build")));
+        this.app.get('/new_frontend/*', (req, res) => {
+            res.sendFile(path.join(__dirname, "../../..", "frontend/build/index.html"));
+        });
 
         this.app.get("/api/v2", (req, res) => {
             let endpoints = listEndpoints(this.app);
