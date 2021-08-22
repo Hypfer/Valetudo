db985f39fcfdf1bc9048d50a3e2d7d75bc42f3da
Sören Beye <github@hypfer.de>
2021-08-22 15:24:08 +0200

    fix(ui): Only dark mode (for now?)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {createTheme, CssBaseline, ThemeProvider, useMediaQuery,} from '@material-ui/core';
+import {createTheme, CssBaseline, ThemeProvider, /*useMediaQuery,*/} from '@material-ui/core';
 import AppRouter from './AppRouter';
 import CapabilitiesProvider from './CapabilitiesProvider';
 import {SnackbarProvider} from 'notistack';
@@ -9,13 +9,13 @@ import {ReactQueryDevtools} from 'react-query/devtools';
 const queryClient = new QueryClient();
 
 const App = (): JSX.Element => {
-    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+    //const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
 
     const theme = React.useMemo(
         () =>
             {return createTheme({
                 palette: {
-                    mode: prefersDarkMode ? 'dark' : 'light',
+                    mode: /*prefersDarkMode ? */ 'dark' /*: 'light' */,
                 },
                 map: {
                     floor: '#0076FF',
@@ -27,7 +27,7 @@ const App = (): JSX.Element => {
                     active: {stroke: '#35911A', fill: '#6AF5424C'},
                 },
             })},
-        [prefersDarkMode]
+        [/*prefersDarkMode*/]
     );
 
     return (
