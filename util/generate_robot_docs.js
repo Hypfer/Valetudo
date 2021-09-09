0bf79f85d08f94e4d2cb66879cef0f9fa3e98042
Sören Beye <github@hypfer.de>
2021-09-09 19:13:28 +0200

    docs: Add model-specific information to robot overview

--- util/generate_robot_docs.js
+++ util/generate_robot_docs.js
@@ -48,6 +48,175 @@ process.on("uncaughtException", function(err) {
     }
 });
 
+const VALETUDO_SUPPORT_GRADES = {
+    GREAT: "great",
+    GOOD: "good",
+    OKAY: "okay",
+    MEH: "meh",
+    BAD: "bad"
+}
+
+const DEVELOPER_SUPPORT_GRADES = {
+    YES: "yes",
+    BEST_EFFORT: "best effort",
+    SOME_EFFORT: "some effort",
+    NONE: "none"
+}
+
+const BUY_GRADES = {
+    GET_IT_RIGHT_NOW: "get it right now!",
+    OKAY: "Can't go wrong with this model",
+    OKAY_ISH: "This model is okay but has some issues that keep it from being fully recommendable",
+    NOT_OKAY: "This model has issues and therefore isn't recommended (see comment)",
+    OUTDATED_OKAY: "outdated but still okay-ish",
+    OUTDATED_NOT_OKAY: "outdated. not recommended (anymore)"
+}
+
+const ModelDescriptions = {
+    "Dreame": {
+        "1C": {
+            valetudoSupport: VALETUDO_SUPPORT_GRADES.OKAY,
+            developerSupport: DEVELOPER_SUPPORT_GRADES.BEST_EFFORT,
+            testedWorking: true,
+            recommended: BUY_GRADES.OUTDATED_NOT_OKAY,
+            comment: "vSLAM, a small battery and an outdated software stack"
+        },
+        "1T": {
+            valetudoSupport: VALETUDO_SUPPORT_GRADES.GOOD,
+            developerSupport: DEVELOPER_SUPPORT_GRADES.BEST_EFFORT,
+            testedWorking: true,
+            recommended: BUY_GRADES.OKAY_ISH,
+            comment: "vSLAM :("
+        },
+        "D9": {
+            valetudoSupport: VALETUDO_SUPPORT_GRADES.GOOD,
+            developerSupport: DEVELOPER_SUPPORT_GRADES.YES,
+            testedWorking: true,
+            recommended: BUY_GRADES.OKAY_ISH,
+            comment: "256MB RAM are problematic when dealing with large floorplans"
+        },
+        "F9": {
+            valetudoSupport: VALETUDO_SUPPORT_GRADES.GOOD,
+            developerSupport: DEVELOPER_SUPPORT_GRADES.BEST_EFFORT,
+            testedWorking: true,
+            recommended: BUY_GRADES.OKAY_ISH,
+            comment: "vSLAM :("
+        },
+        "L10 Pro": {
+            valetudoSupport: VALETUDO_SUPPORT_GRADES.GOOD,
+            developerSupport: DEVELOPER_SUPPORT_GRADES.YES,
+            testedWorking: true,
+            recommended: BUY_GRADES.OKAY,
+            comment: "None"
+        },
+        "MOVA Z500": {
+            valetudoSupport: VALETUDO_SUPPORT_GRADES.GOOD,
+            developerSupport: DEVELOPER_SUPPORT_GRADES.BEST_EFFORT,
+            testedWorking: true,
+            recommended: BUY_GRADES.OKAY_ISH,
+            comment: "vSLAM :("
+        },
+        "Z10 Pro": {
+            valetudoSupport: VALETUDO_SUPPORT_GRADES.GOOD,
+            developerSupport: DEVELOPER_SUPPORT_GRADES.YES,
+            testedWorking: true,
+            recommended: BUY_GRADES.GET_IT_RIGHT_NOW,
+            comment: "The auto-empty-dock is a neat addition"
+        },
+    },
+    "Roborock": {
+        "S4 Max": {
+            valetudoSupport: VALETUDO_SUPPORT_GRADES.OKAY,
+            developerSupport: DEVELOPER_SUPPORT_GRADES.SOME_EFFORT,
+            testedWorking: true,
+            recommended: BUY_GRADES.NOT_OKAY,
+            comment: "No one bothered to further look into newer roborock models meaning that some map-related features are more or less broken\n\nAlso, 256MB RAM and NAND are pretty bad HW specs which can cause issues."
+        },
+        "S4": {
+            valetudoSupport: VALETUDO_SUPPORT_GRADES.OKAY,
+            developerSupport: DEVELOPER_SUPPORT_GRADES.SOME_EFFORT,
+            testedWorking: true,
+            recommended: BUY_GRADES.NOT_OKAY,
+            comment: "No one bothered to further look into newer roborock models meaning that some map-related features are more or less broken"
+        },
+        "S5 Max": {
+            valetudoSupport: VALETUDO_SUPPORT_GRADES.OKAY,
+            developerSupport: DEVELOPER_SUPPORT_GRADES.SOME_EFFORT,
+            testedWorking: true,
+            recommended: BUY_GRADES.NOT_OKAY,
+            comment: "No one bothered to further look into newer roborock models meaning that some map-related features are more or less broken\n\nAlso, 256MB RAM and NAND are pretty bad HW specs which can cause issues."
+        },
+        "S5": {
+            valetudoSupport: VALETUDO_SUPPORT_GRADES.OKAY,
+            developerSupport: DEVELOPER_SUPPORT_GRADES.YES,
+            testedWorking: true,
+            recommended: BUY_GRADES.OUTDATED_OKAY,
+            comment: "Still works finefor most use-cases"
+        },
+        "S6 MaxV": {
+            valetudoSupport: VALETUDO_SUPPORT_GRADES.BAD,
+            developerSupport: DEVELOPER_SUPPORT_GRADES.NONE,
+            testedWorking: true,
+            recommended: BUY_GRADES.NOT_OKAY,
+            comment: "It's basically impossible to root this thing."
+        },
+        "S6 Pure": {
+            valetudoSupport: VALETUDO_SUPPORT_GRADES.OKAY,
+            developerSupport: DEVELOPER_SUPPORT_GRADES.SOME_EFFORT,
+            testedWorking: true,
+            recommended: BUY_GRADES.NOT_OKAY,
+            comment: "No one bothered to further look into newer roborock models meaning that some map-related features are more or less broken\n\nAlso, 256MB RAM and NAND are pretty bad HW specs which can cause issues."
+        },
+        "S6": {
+            valetudoSupport: VALETUDO_SUPPORT_GRADES.OKAY,
+            developerSupport: DEVELOPER_SUPPORT_GRADES.YES,
+            testedWorking: true,
+            recommended: BUY_GRADES.OUTDATED_OKAY,
+            comment: "Still works finefor most use-cases"
+        },
+        "S7": {
+            valetudoSupport: VALETUDO_SUPPORT_GRADES.BAD,
+            developerSupport: DEVELOPER_SUPPORT_GRADES.NONE,
+            testedWorking: true,
+            recommended: BUY_GRADES.NOT_OKAY,
+            comment: "This robot is very expensive while also featuring incredibly weak hardware, which is something that I personally do not want to support."
+        },
+        "Xiaomi Mi Robot Vacuum": {
+            valetudoSupport: VALETUDO_SUPPORT_GRADES.OKAY,
+            developerSupport: DEVELOPER_SUPPORT_GRADES.SOME_EFFORT,
+            testedWorking: true,
+            recommended: BUY_GRADES.OUTDATED_NOT_OKAY,
+            comment: "Unfortunately, this model is lacking basic features such as a persistent map which is insufficient in 2021+"
+        }
+    },
+    "Viomi": {
+        "Xiaomi Mi Robot Vacuum-Mop P": {
+            valetudoSupport: VALETUDO_SUPPORT_GRADES.MEH,
+            developerSupport: DEVELOPER_SUPPORT_GRADES.NONE,
+            testedWorking: true,
+            recommended: BUY_GRADES.NOT_OKAY,
+            comment: "This model is actually just a White-Label Product with a custom Miio Software stack which is EOL and therefore doesn't receive any meaningful software updates.\n\nOverall, it's just weird and annoying."
+        }
+    }
+}
+
+function getModelDescription(vendor, model) {
+    const description = ModelDescriptions[vendor]?.[model];
+
+    if(!description) {
+        throw new Error(`Missing description for ${vendor} ${model}`)
+    }
+
+    return [
+        `#### Valetudo Support\n\n${description.valetudoSupport}\n\n`,
+        `#### Developer Support\n\n${description.developerSupport}\n\n`,
+        "#### Tested Working\n\n" + (description.testedWorking ? "✔" : "❌") + "\n\n",
+        `#### Recommended\n\n${description.recommended}\n\n`,
+        `#### Comment\n\n${description.comment}\n\n`
+    ]
+}
+
+
 const vendors = {};
 
 Object.values(Robots).forEach(robotClass => {
@@ -62,6 +231,7 @@ Object.values(Robots).forEach(robotClass => {
         vendors[instance.getManufacturer()] = vendors[instance.getManufacturer()] ? vendors[instance.getManufacturer()] : {};
 
         vendors[instance.getManufacturer()][instance.constructor.name] = {
+            vendorName: instance.getManufacturer(),
             modelName: instance.getModelName(),
             capabilities: Object.keys(instance.capabilities).sort()
         }
@@ -90,6 +260,7 @@ If you can't find it there, it's most likely not possible (yet?).
 Another source is [https://dontvacuum.me/robotinfo/](https://dontvacuum.me/robotinfo/). Search for "Root method public?".
 If it's listed as "no", then it's certainly not possible for you to run Valetudo on it.
 
+There's also some more information regarding whether or not you should buy a specific robot below the table.
 
 Again:<br/>
 This is just an autogenerated overview based on the codebase at the time of generation.<br/>
@@ -131,7 +302,9 @@ Object.keys(vendors).filter(v => v !== "Valetudo").sort().forEach((vendor, i) =>
         vendorSection.push(
             "### " + robot.modelName + '<a id="'+robotAnchor+'"></a>',
             "",
-            "This model supports the following capabilities:"
+            getModelDescription(robot.vendorName, robot.modelName).join("\n\n"),
+            "",
+            "#### This model supports the following capabilities:"
         );
 
         robot.capabilities.forEach(capability => {
