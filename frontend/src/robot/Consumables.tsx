bfb70b77e5a4d0f4dbfd4ae2c0d6f4e4e940e98e
Christian Friedrich Coors <me@ccoors.de>
2021-09-25 23:38:15 +0200

    feat(ui): Consumables bottom view sensors

--- frontend/src/robot/Consumables.tsx
+++ frontend/src/robot/Consumables.tsx
@@ -103,11 +103,13 @@ const BottomConsumables: FunctionComponent<ConsumableVisualizerProps> = ({
     const [hasMainBrush, highlightMainBrush] = useConsumable(consumables, selectedConsumable, "brush", "main");
     const [hasLeftSideBrush, highlightLeftSideBrush] = useConsumable(consumables, selectedConsumable, "brush", "side_left");
     const [hasRightSideBrush, highlightRightSideBrush] = useConsumable(consumables, selectedConsumable, "brush", "side_right");
+    const [hasSensor, highlightSensor] = useConsumable(consumables, selectedConsumable, "sensor", "all");
 
     const mopFill = hasMop ? (highlightMop ? highlightFill : "#959595") : bodyColor;
     const mainBrushFill = highlightMainBrush ? highlightFill : "#d45500";
     const leftSideBrushFill = highlightLeftSideBrush ? highlightFill : "#808080";
     const rightSideBrushFill = highlightRightSideBrush ? highlightFill : "#808080";
+    const sensorFill = highlightSensor ? highlightFill : "#000";
 
     return (
         <svg viewBox="0 0 512 512" width="100%">
@@ -136,6 +138,23 @@ const BottomConsumables: FunctionComponent<ConsumableVisualizerProps> = ({
                     d="m247.548 53.149s-2.484.083-2.594 9.956c-.11 9.877 2.594 9.96 2.594 9.96h5.314 6.27 5.313s2.484-.083 2.595-9.96c.11-9.877-2.595-9.956-2.595-9.956h-5.313-6.27z"/>
                 <path d="m90.691 201.872h37.182v112.309h-37.182z" stroke="#ccc"/>
                 <path d="m384.12 201.872h37.183v112.309h-37.183z" stroke="#ccc"/>
+
+                {hasSensor && (
+                    <g fill={sensorFill}>
+                        <path
+                            stroke={strokeColor}
+                            d="M 177.33 47.7031 L 214.64 37.7059 C 217.442 36.9554 220.301 38.6061 221.051 41.4072 L 223.284 49.7396 C 224.035 52.5406 222.384 55.3999 219.583 56.1504 L 182.273 66.1477 C 179.471 66.8982 176.612 65.2474 175.862 62.4463 L 173.629 54.114 C 172.878 51.3129 174.529 48.4537 177.33 47.7031 Z" />
+                        <path
+                            stroke={strokeColor}
+                            d="M 292.403 56.1504 L 329.714 66.1476 C 332.515 66.8982 335.374 65.2474 336.124 62.4463 L 338.357 54.114 C 339.108 51.3129 337.457 48.4536 334.656 47.7031 L 297.346 37.7059 C 294.544 36.9553 291.685 38.6061 290.935 41.4072 L 288.702 49.7396 C 287.952 52.5406 289.602 55.3999 292.403 56.1504 Z" />
+                        <path
+                            stroke={strokeColor}
+                            d="M 440.071 169.576 L 453.318 205.86 C 454.312 208.584 457.306 209.976 460.03 208.982 L 468.133 206.023 C 470.857 205.029 472.249 202.035 471.255 199.311 L 458.008 163.027 C 457.013 160.303 454.02 158.911 451.296 159.905 L 443.193 162.864 C 440.469 163.858 439.076 166.852 440.071 169.576 Z" />
+                        <path
+                            stroke={strokeColor}
+                            d="M 40.7311 199.311 L 53.9779 163.027 C 54.9724 160.303 57.966 158.911 60.69 159.906 L 68.7932 162.864 C 71.5172 163.859 72.9096 166.852 71.915 169.576 L 58.6682 205.86 C 57.6737 208.584 54.6801 209.976 51.9561 208.982 L 43.8529 206.023 C 41.1289 205.029 39.7366 202.035 40.7311 199.311 Z" />
+                    </g>
+                )}
             </g>
         </svg>
     );
