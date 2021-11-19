02e155cec954cc75e41c5fcbf8ebe37acffb4c37
Alexandr Zolotukhin <supersmile2009@gmail.com>
2021-11-19 17:24:56 +0200

    feat(vendor.roborock): RoborockKeyLockCapability (#1222)

---
+++ backend/lib/robots/roborock/capabilities/RoborockKeyLockCapability.js
@@ -0,0 +1,34 @@
+const KeyLockCapability = require("../../../core/capabilities/KeyLockCapability");
+
+/**
+ * @extends KeyLockCapability<import("../RoborockValetudoRobot")>
+ */
+class RoborockKeyLockCapability extends KeyLockCapability {
+
+    /**
+     * This function polls the current key lock state
+     *
+     * @returns {Promise<boolean>}
+     */
+    async isEnabled() {
+        const res = await this.robot.sendCommand("get_child_lock_status", [], {});
+
+        return res["lock_status"] === 1;
+    }
+
+    /**
+     * @returns {Promise<void>}
+     */
+    async enable() {
+        await this.robot.sendCommand("set_child_lock_status", {"lock_status": 1}, {});
+    }
+
+    /**
+     * @returns {Promise<void>}
+     */
+    async disable() {
+        await this.robot.sendCommand("set_child_lock_status", {"lock_status": 0}, {});
+    }
+}
+
+module.exports = RoborockKeyLockCapability;
