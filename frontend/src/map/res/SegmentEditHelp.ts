17e1d5ffaa0d938317699b89a2244541dd9dad14
Sören Beye <github@hypfer.de>
2021-12-30 10:18:29 +0100

    feat(ui): Improve segment edit help

--- frontend/src/map/res/SegmentEditHelp.ts
+++ frontend/src/map/res/SegmentEditHelp.ts
@@ -24,13 +24,15 @@ can distinguish them from each other. From time to time segments might also chan
 You can only edit segments if there are any. If you only see a blue map then you have no segments.
 
 Make sure that you've done a full cleanup task with the robot returning to its dock on its own without any interruption.
-This is usually required for the robot to split the map into segments. 
+This is usually required for the robot to split the map into segments.
+
+Also, some firmwares might need you to manually enable the saving of persistent maps before starting that full cleanup task.
 
 #### I can't split a segment
 
 Sometimes, the cutting line placement needs some wiggling to work.
 It is not required to carefully place the cutting line exactly between the wall pixels. In fact, this often prevents
-a successful split.
+a successful split. Try dragging it over the whole width/height of the segment instead of just parts of it.
 
 In some room layouts, you also might have to split a segment multiple times and then rejoin some of those parts to get 
 the desired result.
