3673edcbd3e0f0c7cbc98fd2b4e534154faab2ee
Sören Beye <github@hypfer.de>
2022-04-15 11:40:03 +0200

    chore: Some windows test case weirdness

--- backend/test/lib/utils/LinuxToolsHelper_spec.js
+++ backend/test/lib/utils/LinuxToolsHelper_spec.js
@@ -9,7 +9,7 @@ should.config.checkProtoEql = false;
 describe("LinuxToolsHelper", function () {
 
     it("Should parse /proc/meminfo Kernel 3.4.39 from roborock s5 correctly", async function() {
-        let data = fs.readFileSync(path.join(__dirname, "/res/meminfo_3.4.39_roborock_s5.txt")).toString();
+        let data = fs.readFileSync(path.join(__dirname, "/res/meminfo_3.4.39_roborock_s5.txt")).toString().replaceAll("\r", "");
         let expected = JSON.parse(fs.readFileSync(path.join(__dirname, "/res/meminfo_3.4.39_roborock_s5.json")).toString());
 
         let actual = LinuxToolsHelper.PARSE_PROC_MEMINFO(data);
@@ -18,7 +18,7 @@ describe("LinuxToolsHelper", function () {
     });
 
     it("Should parse /proc/meminfo Kernel 3.4.39 from viomi v7 correctly", async function() {
-        let data = fs.readFileSync(path.join(__dirname, "/res/meminfo_3.4.39_viomi_v7.txt")).toString();
+        let data = fs.readFileSync(path.join(__dirname, "/res/meminfo_3.4.39_viomi_v7.txt")).toString().replaceAll("\r", "");
         let expected = JSON.parse(fs.readFileSync(path.join(__dirname, "/res/meminfo_3.4.39_viomi_v7.json")).toString());
 
         let actual = LinuxToolsHelper.PARSE_PROC_MEMINFO(data);
@@ -27,7 +27,7 @@ describe("LinuxToolsHelper", function () {
     });
 
     it("Should parse /proc/meminfo Kernel 4.9.191 from dreame z10 correctly", async function() {
-        let data = fs.readFileSync(path.join(__dirname, "/res/meminfo_4.9.191_dreame_z10.txt")).toString();
+        let data = fs.readFileSync(path.join(__dirname, "/res/meminfo_4.9.191_dreame_z10.txt")).toString().replaceAll("\r", "");
         let expected = JSON.parse(fs.readFileSync(path.join(__dirname, "/res/meminfo_4.9.191_dreame_z10.json")).toString());
 
         let actual = LinuxToolsHelper.PARSE_PROC_MEMINFO(data);
