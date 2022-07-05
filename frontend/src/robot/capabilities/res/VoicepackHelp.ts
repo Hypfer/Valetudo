05a6b89848b5c1e9a04f4a822da58e03cd89b5f8
Sören Beye <github@hypfer.de>
2022-07-05 20:53:47 +0200

    chore: Update wording in VoicepackHelp.ts
    Minor corrections.
    
    Co-authored-by: Turbotomate <64365398+Turbotomate@users.noreply.github.com>

--- frontend/src/robot/capabilities/res/VoicepackHelp.ts
+++ frontend/src/robot/capabilities/res/VoicepackHelp.ts
@@ -7,7 +7,7 @@ As I personally don't really care about them, this feature is a bit rough around
 For me, these things are first and foremost tools that should simply work and do so reliably.
 Thus, I'm fine with it just talking english as that's perfectly serviceable for a tool.
 
-If you want to install install a custom one anyways, you will need a voicepack in the correct format as required by your
+If you want to install a custom one anyways, you will need a voicepack in the correct format as required by your
 robot hosted on some http server. Note that the official CDN is often blocked in the robots firmware meaning that just
 using the official URL likely won't work.
 
@@ -17,6 +17,6 @@ The type of hash differs depending on the firmware.
 Furthermore, you'll also need to specify a language code. That value largely doesn't matter much unless you use a reserved code.
 "EN", "CN" etc are often codes reserved for the inbuilt voice packs of the firmware.
 
-Usually it's possible to revert to the currently installed voice pack by specifying one of those inbuilt voicepack codes during the install.
+Usually it's possible to revert to the inititally installed voice pack by specifying one of those inbuilt voicepack codes during the install.
 
 `;
