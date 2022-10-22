040d4fb2a616c36e572aaf536c1f2cc419a0d705
Sören Beye <github@hypfer.de>
2022-10-22 17:44:44 +0200

    chore(vendor.dreame): Add test case map with left cutoff by firmware

--- backend/test/lib/robots/dreame/DreameMapParser_spec.js
+++ backend/test/lib/robots/dreame/DreameMapParser_spec.js
@@ -338,4 +338,28 @@ describe("DreameMapParser", function () {
 
         actual.should.deepEqual(expected);
     });
+
+    it("Should pre-process & parse monastery map with left cutoff correctly", async function() {
+        let data = await fs.readFile(path.join(__dirname, "/res/map/misc_monastery_with_cutoff.bin"));
+        let expected = JSON.parse(await fs.readFile(path.join(__dirname, "/res/map/misc_monastery_with_cutoff.json"), { encoding: "utf-8" }));
+        let actual = await DreameMapParser.PARSE(await DreameMapParser.PREPROCESS(data));
+
+        if (actual.metaData?.nonce) {
+            delete(actual.metaData.nonce);
+        }
+
+        actual.layers.length.should.equal(expected.layers.length, "layerCount");
+
+        actual.layers.forEach((layer, i) => {
+            actual.layers[i].should.deepEqual(expected.layers[i]);
+        });
+
+        actual.entities.length.should.equal(expected.entities.length, "entitiesCount");
+
+        actual.entities.forEach((layer, i) => {
+            actual.entities[i].should.deepEqual(expected.entities[i]);
+        });
+
+        actual.should.deepEqual(expected);
+    });
 });
