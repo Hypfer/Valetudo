09ef3a139139e7b6ca70c20e8f89f74237bfa974
Florian Windhagauer <fwindhagauer@eurofunk.com>
2022-12-04 21:14:39 +0100

    Add implementations for Roborock S7 auto empty dock

--- backend/lib/robots/roborock/RoborockQuirkFactory.js
+++ backend/lib/robots/roborock/RoborockQuirkFactory.js
@@ -14,6 +14,57 @@ class RoborockQuirkFactory {
      */
     getQuirk(id) {
         switch (id) {
+            case RoborockQuirkFactory.KNOWN_QUIRKS.AUTO_EMPTY_LENGTH:
+                return new Quirk({
+                    id: id,
+                    title: "Auto Empty Duration",
+                    description: "Set the dustbin emptying duration when triggered by robot after docking.",
+                    options: ["Smart", "Quick", "Daily", "Max"],
+                    getter: async() => {
+                        const res = await this.robot.sendCommand("get_dust_collection_mode", [], {});
+
+                        if (!(res && res.mode !== undefined)) {
+                            throw new Error(`Received invalid response: ${res}`);
+                        } else {
+                            switch (res.mode) {
+                                case 4:
+                                    return "Max";
+                                case 2:
+                                    return "Daily";
+                                case 1:
+                                    return "Quick";
+                                case 0:
+                                    return "Smart";
+                                default:
+                                    throw new Error(`Received invalid value ${res.status}`);
+                            }
+                        }
+
+
+                    },
+                    setter: async(value) => {
+                        let val;
+
+                        switch (value) {
+                            case "Max":
+                                val = 4;
+                                break;
+                            case "Daily":
+                                val = 2;
+                                break;
+                            case "Quick":
+                                val = 1;
+                                break;
+                            case "Smart":
+                                val = 0;
+                                break;
+                            default:
+                                throw new Error(`Received invalid value ${value}`);
+                        }
+
+                        return this.robot.sendCommand("set_dust_collection_mode", { "mode": val }, {});
+                    }
+                });
             case RoborockQuirkFactory.KNOWN_QUIRKS.BUTTON_LEDS:
                 return new Quirk({
                     id: id,
@@ -103,6 +154,7 @@ class RoborockQuirkFactory {
 }
 
 RoborockQuirkFactory.KNOWN_QUIRKS = {
+    AUTO_EMPTY_LENGTH: "7e33281f-d1bd-4e11-a100-b2c792284883",
     BUTTON_LEDS: "57ffd1d3-306e-4451-b89c-934ec917fe7e",
     STATUS_LED: "1daf5179-0689-48a5-8f1b-0a23e11836dc"
 };
