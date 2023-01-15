bebf0a4cc0921f3dd3c5cdfb0875ffa77743e3c3
Sören Beye <github@hypfer.de>
2023-01-15 11:48:39 +0100

    chore: Formatting

--- backend/lib/webserver/WebServer.js
+++ backend/lib/webserver/WebServer.js
@@ -245,6 +245,7 @@ class WebServer {
             spec = JSON.parse(fs.readFileSync(path.join(__dirname, "../res/valetudo.openapi.schema.json")).toString());
         } catch (e) {
             Logger.warn("Failed to load OpenApi spec. Swagger endpoint and payload validation will be unavailable.", e.message);
+
             return;
         }
 
