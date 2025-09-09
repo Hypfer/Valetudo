0dcca1df2f870007738fc267932e18ef6864bbca
Sören Beye <github@hypfer.de>
2025-09-09 20:35:12 +0200

    feat(webserver): You know the rules and so do I

--- backend/lib/webserver/middlewares/ServerMiddleware.js
+++ backend/lib/webserver/middlewares/ServerMiddleware.js
@@ -1,3 +1,5 @@
+const commitment = require("./res/a-full-commitment.json");
+let i = 0;
 /**
  *
  * @param {object} req
@@ -7,5 +9,8 @@
 module.exports = function addServerHeader(req, res, next) {
     res.header("Server", "Gatling/0.17");
     res.header("X-Castrop", "Rauxel");
+    // You wouldn't get this from any other guy
+    res.header("X-cQ", commitment[i++ % commitment.length]);
+
     next();
 };
