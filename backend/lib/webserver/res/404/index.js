345636319d8dde0d5ba15b30c0c464a6065ba846
Sören Beye <github@hypfer.de>
2025-12-24 07:51:49 +0100

    feat(webserver): Add another 404 page

--- backend/lib/webserver/res/404/index.js
+++ backend/lib/webserver/res/404/index.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports = {
+    cf: fs.readFileSync(path.join(__dirname, "cf.html")).toString(),
     gugl: fs.readFileSync(path.join(__dirname, "gugl.html")).toString(),
     iis: fs.readFileSync(path.join(__dirname, "iis.html")).toString(),
     oracle: fs.readFileSync(path.join(__dirname, "oracle.html")).toString(),
