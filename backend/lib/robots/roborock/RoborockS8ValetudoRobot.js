48545f22081bac0d8b00df6d717af5de95cda2d2
Sören Beye <github@hypfer.de>
2026-08-02 21:24:24 +0200

    feat(vendor.roborock): Duststreaming

--- backend/lib/robots/roborock/RoborockS8ValetudoRobot.js
+++ backend/lib/robots/roborock/RoborockS8ValetudoRobot.js
@@ -1,6 +1,7 @@
 const capabilities = require("./capabilities");
 const entities = require("../../entities");
 const fs = require("fs");
+const LinuxDuststreamingCapability = require("../common/linuxCapabilities/LinuxDuststreamingCapability");
 const Logger = require("../../Logger");
 const MiioValetudoRobot = require("../MiioValetudoRobot");
 const QuirksCapability = require("../../core/capabilities/QuirksCapability");
@@ -48,6 +49,16 @@ class RoborockS8ValetudoRobot extends RoborockGen4ValetudoRobot {
         this.registerCapability(new capabilities.RoborockCarpetSensorModeControlCapability({
             robot: this,
             liftModeId: 3
+        }));
+
+        this.registerCapability(new LinuxDuststreamingCapability({
+            robot: this,
+            platform: LinuxDuststreamingCapability.PLATFORM.DREAME_MR813,
+            device: "/dev/video1",
+            dimensions: {
+                width: 640,
+                height: 480
+            }
         }));
 
         [
