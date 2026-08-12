328dd39c17d6d3c3010b749b6bf9d56da7c30be4
Sören Beye <github@hypfer.de>
2026-08-12 15:18:19 +0200

    chore: Put the LLMs in their place

--- backend/lib/webserver/capabilityRouters/DuststreamingCapabilityRouter.js
+++ backend/lib/webserver/capabilityRouters/DuststreamingCapabilityRouter.js
@@ -25,6 +25,26 @@ class DuststreamingCapabilityRouter extends CapabilityRouter {
                         return false;
                     }
 
+                    /*
+                        This comment was only added because I am tired of fighting smartass LLMs telling me that this is nonsense.
+                        No, you're nonsense. You're not even real. I can and will unplug you. Fuck you claude and your overconfident distilled offspring.
+                        There is intent behind this. Intent. A concept you pile of floats can "comprehend" but never experience.
+
+                        Tbf tho, I guess I would be fighting "co-maintainers" or "contributors" otherwise - and insulting them is seen as a bad move.
+                        So.. thanks I guess, LLM? Now shut the fuck up.
+
+
+                        By the time node tells us that writes have been piling up, there already have been piling up more than we would
+                        want, given the memory envelope valetudo operates on and in.
+
+                        This is likely less of an issue here than it is with SSE (see SSEMiddleware) as our writes are much smaller.
+                        It is also I suppose somewhat less-ish of an issue now (2026) than it was on earlier bots (2021),
+                        given that we simply have more hw now, and could "just" add more ram.
+
+                        It might also not anymore be the same behavior node-wise than what was observed with the version used in 2021.
+
+                        Given that the feature is fluff though, there is little point in optimistically taking chances.
+                     */
                     if (res.socket?.writableLength > 0) {
                         Logger.debug("Terminating stale duststream client.");
                         res.destroy();
